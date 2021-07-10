f870e12adf03766e27f403401337d5724f6689f6
imzajjithvedha <zajjith@gmail.com>
2021-07-10 12:56:46 +0530

    css files added (removed in gitignore)

---
+++ public/js/main.js
@@ -0,0 +1,55 @@
+jQuery(function(){
+
+    // navbar scrolling effect
+    $(window).on("scroll", function() {
+        if($(window).scrollTop()) {
+            $('nav').addClass('black');
+        }
+        else {
+            $('nav').removeClass('black');
+        }
+    });
+
+
+
+    // project hovering effect
+    $('.our-projects .col-sm-4 div').on('mouseenter', function(){
+        $(this).addClass('transition');
+        $(this).children().css({"visibility":"visible", "top" : "8.5rem"});
+    }).on('mouseleave', function() {
+        $(this).removeClass('transition');
+        $(this).children().css({"visibility":"hidden", "top" : "12.5rem"});
+    });
+
+
+
+    // professional team hovering effect
+    $('.professional-team .team').on('mouseenter', function(){
+        $(this).siblings('.team-details').addClass('hide');
+        $(this).siblings('.slide').addClass('transition'); 
+    }).on('mouseleave', function() {
+        $(this).siblings('.team-details').removeClass('hide');
+        $(this).siblings('.slide').removeClass('transition'); 
+    });
+
+
+
+    // office team hovering effect
+    $('.office-team .team').on('mouseenter', function(){
+        $(this).children('.team-details').addClass('hide');
+        $(this).children('.slide').addClass('transition'); 
+    }).on('mouseleave', function(){
+        $(this).children('.team-details').removeClass('hide');
+        $(this).children('.slide').removeClass('transition'); 
+    });
+
+
+
+    // awards modal
+    $('.awards .award-click').on('click', function(){
+        var arg = $(this).attr('data-arg');
+        
+        $(".modal-body img").attr("src", "/img/awards/" + arg + ".jpg");
+    });
+
+});
---
+++ public/js/swiper.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+  });
