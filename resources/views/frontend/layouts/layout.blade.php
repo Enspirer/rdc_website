e78912ce1ed5fbfc6ac884f75d69cf5b698ee9a9
sanjayaharshana <sanjayaharasana@gmail.com>
2021-10-15 13:38:43 +0530

    RDC Function

--- resources/views/frontend/layouts/layout.blade.php
+++ resources/views/frontend/layouts/layout.blade.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title> @yield('title')</title>
+    <title> @yield('title') RDC Architects</title>
     <meta name="description" content="@yield('meta_description')">
     <meta name="keywords" content="@yield('meta_keywords')">
 
